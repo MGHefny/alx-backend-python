16416c896821ed7aff36b5ac2170f4b092034a5f
MGHefny <mgenedhefny@gmail.com>
2024-08-06 19:47:04 +0300

    2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python3
-"""Task 2"""
+"""#!/usr/bin/env python3
+""""""Task 2""""""
 import asyncio
 import time
 
@@ -7,12 +7,21 @@ async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
 async def measure_runtime() -> float:
-    """4 times and execution time"""
+    """""""4 times and execution time"""""""
     start = time.time()
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-    )
-    return time.time() - start
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    return time.time() - start"""
+
+#!/usr/bin/env python3
+"""tsk2 time"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """test"""
+    start_time = time.perf_counter()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension())
+    return time.perf_counter() - start_time
