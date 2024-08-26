86dcbe968d3c1c434693cdb0c8fa289ccb94fe3a
MGHefny <mgenedhefny@gmail.com>
2024-08-26 21:05:03 +0300

    4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -8,16 +8,13 @@ from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """ cls test git hub org """
+    """cls test git hub org"""
 
-    @parameterized.expand([
-        ('google'),
-        ('abc')
-    ])
-    @patch('client.get_json')
+    @parameterized.expand([("google"), ("abc")])
+    @patch("client.get_json")
     def test_org(self, data_in, get_mock):
-        """ correct result from test"""
+        """correct result from test"""
         cl_test = GithubOrgClient(data_in)
         cl_test.org()
-        the_url = f'https://api.github.com/orgs/{org_name}'
+        the_url = f"https://api.github.com/orgs/{org_name}"
         mock_get_json.assert_called_once_with(the_url)
