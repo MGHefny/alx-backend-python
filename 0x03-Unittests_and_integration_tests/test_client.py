63d86c519c47a4b7a8d0fee8303506145444c6d4
MGHefny <mgenedhefny@gmail.com>
2024-08-26 21:04:02 +0300

    4

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" task test client """
+
+import unittest
+from unittest.get_mock import patch
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ cls test git hub org """
+
+    @parameterized.expand([
+        ('google'),
+        ('abc')
+    ])
+    @patch('client.get_json')
+    def test_org(self, data_in, get_mock):
+        """ correct result from test"""
+        cl_test = GithubOrgClient(data_in)
+        cl_test.org()
+        the_url = f'https://api.github.com/orgs/{org_name}'
+        mock_get_json.assert_called_once_with(the_url)
