5fe5d1778b2b44cff271dbf97b4f8cc1eaef2832
MGHefny <mgenedhefny@gmail.com>
2024-08-26 21:11:08 +0300

    4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 
 import unittest
 from unittest.mock import patch
-from parameterized import parameterized
+from parameterized import parameterized, parameterized_class
 from client import GithubOrgClient
 
 
