b1e592a002cb8081068db08ae4e630b1517e13f6
MGHefny <mgenedhefny@gmail.com>
2024-08-26 22:47:10 +0300

    67

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -16,14 +16,14 @@ class TestGithubOrgClient(unittest.TestCase):
     ])
     @patch('client.get_json')
     def test_org(self, data_in, mock_g):
-        """ correct result from test """
+        """ correct output from test """
         cl_test = GithubOrgClient(data_in)
         cl_test.org()
         the_url = f"https://api.github.com/orgs/{data_in}"
         mock_g.assert_called_once_with(the_url)
 
     def test_public_repos_url(self):
-        """ """
+        """ test pub repo """
         with patch('client.GithubOrgClient.org',
                    the_call=PropertyMock) as mock_g:
             pay_load = {"the_url": "World"}
@@ -31,3 +31,32 @@ class TestGithubOrgClient(unittest.TestCase):
             cl_test = GithubOrgClient('check')
             output = cl_test._public_repos_url
             self.assertEqual(output, pay_load["the_url"])
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_g):
+        """ check list of repo
+        check mocked get jison """
+        pay_load_j = [{"name": "Yahoo"}, {"name": "Twitter"}]
+        mock_g.return_value = pay_load_j
+
+        with patch('client.GithubOrgClient._public_repos_url',
+                   the_call=PropertyMock) as mock_p:
+
+            mock_p.return_value = "welcome"
+            test_class = GithubOrgClient('check')
+            output = test_class.public_repos()
+
+            re_check = [x["name"] for x in pay_load_j]
+            self.assertEqual(output, re_check)
+
+            mock_p.assert_called_once()
+            mock_g.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, test_repo, key_l, check_ex):
+        """ test licens """
+        output = GithubOrgClient.has_license(test_repo, key_l)
+        self.assertEqual(output, check_ex)
