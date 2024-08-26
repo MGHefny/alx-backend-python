067965048e9221468517e268fcfc6eb120191fde
MGHefny <mgenedhefny@gmail.com>
2024-08-26 22:01:30 +0300

    5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """ task test client """
 
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized, parameterized_class
 from client import GithubOrgClient
 
@@ -21,3 +21,13 @@ class TestGithubOrgClient(unittest.TestCase):
         cl_test.org()
         the_url = f"https://api.github.com/orgs/{data_in}"
         mock_g.assert_called_once_with(the_url)
+
+    def test_public_repos_url(self):
+        """ """
+        with patch('client.GithubOrgClient.org',
+                   the_call=PropertyMock) as mock_g:
+            pay_load = {"the_url": "World"}
+            mock_g.return_value = pay_load
+            cl_test = GithubOrgClient('check')
+            output = cl_test._public_repos_url
+            self.assertEqual(output, pay_load["the_url"])
